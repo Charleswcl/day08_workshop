73dd0a68617c15dc60f0dc33fe77bd74f9801bb0
Charleswcl <wang90cl@hotmail.com>
2023-01-19 14:30:49 +0800

    Dir & File Creation

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  */
@@ -11,8 +14,9 @@ public final class App {
      * Says hello to the world.
      * 
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // CollectionSortExample cs = new CollectionSortExample();
         // cs.exmaple03();
 
@@ -28,7 +32,36 @@ public final class App {
         // LinkedListExample lle = new LinkedListExample();
         // lle.example();
 
-        StackedExample se = new StackedExample();
-        se.example();
+        // StackedExample se = new StackedExample();
+        // se.example();
+
+        String dirPath = "data";
+        String fileName = "data.txt";
+
+        // create a directory
+        File newDir = new File(dirPath);
+        boolean isDirCreated = newDir.mkdir();
+
+        if (isDirCreated)
+            System.out.println("New Directory " + dirPath + " Create.");
+        else
+            System.out.println("Directory " + dirPath + " Already Existed.");
+
+        // create a file in the directory above
+        File newFile = new File(dirPath + File.separator + fileName);
+        boolean isFileCreated = newFile.createNewFile();
+
+        if (isFileCreated)
+            System.out.println("New file " + fileName + " created");
+        else
+            System.out.println("File " + fileName + " already existed");
+
+        // lsit files under a directory
+        File fileListp[] = newDir.listFiles();
+        for (File f : fileListp) {
+            System.out.println("File " + f.getPath() + ":" + f.getCanonicalPath());
+        }
+
     }
+
 }
