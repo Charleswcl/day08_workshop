c4c92b59b0b7ecc5ffe3dc590185e5d4f4594967
Charleswcl <wang90cl@hotmail.com>
2023-01-20 08:58:53 +0800

    final commit

--- src/main/java/sg/edu/nus/iss/CSVReader.java
+++ src/main/java/sg/edu/nus/iss/CSVReader.java
@@ -1,5 +1,13 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class CSVReader {
+
+    private String file;
+    BufferedReader br = new BufferedReader(new FileReader(employee));
+    
     
 }
